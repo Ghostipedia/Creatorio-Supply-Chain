7df3e25f25eba4ccda4445bcfbabe95380cf4b33
AWildVolt <166110881+AWildVolt@users.noreply.github.com>
2026-04-26 19:14:02 +1200

    add coral smoking recipes

--- overrides/kubejs/server_scripts/Misc.js
+++ overrides/kubejs/server_scripts/Misc.js
@@ -51,5 +51,21 @@ ServerEvents.recipes(event => {
     B: 'drivebywire:wire',
     C: 'create:andesite_casing'
   })
+  //coral smoking
+  event.smoking('minecraft:dead_tube_coral_block', 'minecraft:tube_coral_block')
+  event.smoking('minecraft:dead_brain_coral_block', 'minecraft:brain_coral_block')
+  event.smoking('minecraft:dead_bubble_coral_block', 'minecraft:bubble_coral_block')
+  event.smoking('minecraft:dead_fire_coral_block', 'minecraft:fire_coral_block')
+  event.smoking('minecraft:dead_horn_coral_block', 'minecraft:horn_coral_block')
+  event.smoking('minecraft:dead_tube_coral', 'minecraft:tube_coral')
+  event.smoking('minecraft:dead_tube_coral_fan', 'minecraft:tube_coral_fan')
+  event.smoking('minecraft:dead_brain_coral', 'minecraft:brain_coral')
+  event.smoking('minecraft:dead_brain_coral_fan', 'minecraft:brain_coral_fan')
+  event.smoking('minecraft:dead_bubble_coral', 'minecraft:bubble_coral')
+  event.smoking('minecraft:dead_bubble_coral_fan', 'minecraft:bubble_coral_fan')
+  event.smoking('minecraft:dead_fire_coral', 'minecraft:fire_coral')
+  event.smoking('minecraft:dead_fire_coral_fan', 'minecraft:fire_coral_fan')
+  event.smoking('minecraft:dead_horn_coral', 'minecraft:horn_coral')
+  event.smoking('minecraft:dead_horn_coral_fan', 'minecraft:horn_coral_fan')
 })
 
